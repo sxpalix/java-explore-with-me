5dd9bb438cdf45757f3ee87eb97b703cf8ef6101
sxpalix <vanessfrolov@gmail.com>
2023-11-26 10:15:39 +0300

    "Сервер статистики"

--- ewm-stats/server/src/main/java/ru/practicum/server/repository/StatsRepository.java
+++ ewm-stats/server/src/main/java/ru/practicum/server/repository/StatsRepository.java
@@ -23,7 +23,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
     @Query("SELECT new ru.practicum.model.dto.ViewStats(s.app, s.uri, count(distinct s.ip))" +
             "from Stats s " +
             "where s.timestamp between :start and :end " +
-            "and s.uri in :uris "+
+            "and s.uri in :uris " +
             " group by s.app, s.uri order by count(distinct s.ip) desc")
     List<ViewStats> findAllStatsByTimeAndUrisWhenUniqueTrue(@Param("start") LocalDateTime start,
                                                             @Param("end") LocalDateTime end,
